90a2f40c24f4fbf0849b7fe751c8d1f0ef4fd40b
Ryui-code <ulanbektalasbaev2020@gmail.com>
2025-10-20 21:16:18 +0600

    Hi!

--- Settings/Settings/urls.py
+++ Settings/Settings/urls.py
@@ -24,11 +24,12 @@ from rest_framework import permissions
 from django.conf import settings
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="Movie Site",
-        default_version='v1',),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+   openapi.Info(
+      title="Geo_chat",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = i18n_patterns(
